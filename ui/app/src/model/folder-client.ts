925dcfbc6a705cde283faf09c6dc8c5a2143aca2
rafi <rafialam@appscode.com>
2025-09-24 18:42:10 +0600

    folder delete and update api
    Signed-off-by: rafi <rafialam@appscode.com>

--- ui/app/src/model/folder-client.ts
+++ ui/app/src/model/folder-client.ts
@@ -1,11 +1,12 @@
 import { useMutation, UseMutationResult, useQuery, useQueryClient, UseQueryResult } from '@tanstack/react-query';
-import { HTTPMethodPOST, HTTPHeader, HTTPMethodGET } from './http';
+import { HTTPMethodPOST, HTTPHeader, HTTPMethodGET, HTTPMethodPUT, HTTPMethodDELETE } from './http';
 import buildURL from './url-builder';
 import { fetchJson, FolderResource, DashboardResource, StatusError } from '@perses-dev/core';
 import { useAuthToken } from './auth-client';
 
 export interface FolderWithDashboards extends FolderResource {
   dashboards: DashboardResource[];
+  spec: any;
 }
 
 // change this if Perses uses a different resource name for folders
@@ -32,6 +33,27 @@ export interface FolderListOptions {
   project?: string;
 }
 
+export function updateFolder(owner: string | undefined, entity: FolderResource): Promise<FolderResource> {
+  const name = entity.metadata.name;
+  const project = entity.metadata.project;
+  const url = buildURL({ resource: folderResource, name, project, owner });
+  return fetchJson<FolderResource>(url, {
+    method: HTTPMethodPUT,
+    headers: HTTPHeader,
+    body: JSON.stringify(entity),
+  });
+}
+
+export function deleteFolder(owner: string | undefined, entity: FolderResource): Promise<Response> {
+  const name = entity.metadata.name;
+  const project = entity.metadata.project;
+  const url = buildURL({ resource: folderResource, name, project, owner });
+  return fetch(url, {
+    method: HTTPMethodDELETE,
+    headers: HTTPHeader,
+  });
+}
+
 export function useFolderList(options: FolderListOptions): UseQueryResult<FolderWithDashboards[], StatusError> {
   const { data: decodedToken } = useAuthToken();
   const owner = decodedToken?.sub;
@@ -55,5 +77,42 @@ export function useCreateFolderMutation(
     mutationFn: (folder) => createFolder(owner, folder),
     onSuccess,
     onSettled: () => queryClient.invalidateQueries({ queryKey: [folderResource] }),
+  });
+}
+
+export function useUpdateFolderMutation(): UseMutationResult<FolderResource, StatusError, FolderResource> {
+  const queryClient = useQueryClient();
+  const { data: decodedToken } = useAuthToken();
+  const owner = decodedToken?.sub;
+
+  return useMutation<FolderResource, StatusError, FolderResource>({
+    mutationKey: [folderResource],
+    mutationFn: (folder: FolderResource) => updateFolder(owner, folder),
+    onSuccess: (entity: FolderResource) => {
+      return Promise.all([
+        queryClient.invalidateQueries({ queryKey: [folderResource, entity.metadata.project] }),
+        queryClient.invalidateQueries({ queryKey: [folderResource] }),
+      ]);
+    },
+  });
+}
+
+export function useDeleteFolderMutation(): UseMutationResult<FolderResource, StatusError, FolderResource> {
+  const queryClient = useQueryClient();
+  const { data: decodedToken } = useAuthToken();
+  const owner = decodedToken?.sub;
+
+  return useMutation<FolderResource, StatusError, FolderResource>({
+    mutationKey: [folderResource],
+    mutationFn: async (entity: FolderResource) => {
+      await deleteFolder(owner, entity);
+      return entity;
+    },
+    onSuccess: (entity: FolderResource) => {
+      return Promise.all([
+        queryClient.invalidateQueries({ queryKey: [folderResource, entity.metadata.project] }),
+        queryClient.invalidateQueries({ queryKey: [folderResource] }),
+      ]);
+    },
   });
 }
